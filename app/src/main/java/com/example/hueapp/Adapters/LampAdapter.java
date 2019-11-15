6a288e94306ab47939470c20653a61cea072ea43
Kirsten <kirstabro@gmail.com>
2019-11-15 15:13:03 +0100

    GUI opzet

---
+++ app/src/main/java/com/example/hueapp/Adapters/LampAdapter.java
@@ -0,0 +1,53 @@
+package com.example.hueapp.Adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class LampAdapter extends RecyclerView.Adapter
+{
+    ArrayList<String> dataset = new ArrayList<>();
+
+    public LampAdapter(ArrayList<String> dateset)
+    {
+        this.dataset = dateset;
+    }
+    
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position)
+    {
+
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return 0;
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder
+    {
+
+        public ImageViewHolder(View view)
+        {
+            super(view);
+
+
+
+        }
+    }
+
+}
