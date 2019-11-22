51787c4066f2d6e5ebd51ff27721c7bbcff82e42
Kirsten <kirstabro@gmail.com>
2019-11-22 13:00:31 +0100

    yeet

--- app/src/main/java/com/example/hueapp/Adapters/LampAdapter.java
+++ app/src/main/java/com/example/hueapp/Adapters/LampAdapter.java
@@ -1,11 +1,20 @@
 package com.example.hueapp.Adapters;
 
+import android.graphics.Color;
+import android.nfc.cardemulation.CardEmulation;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.hueapp.HueLamp;
@@ -36,7 +45,8 @@ public class LampAdapter extends RecyclerView.Adapter<LampAdapter.ImageViewHolde
     @Override
     public void onBindViewHolder(@NonNull ImageViewHolder holder, int position)
     {
-
+        float[] hsb = {holder.HueBar.getProgress(), holder.SatBar.getProgress()/100f, holder.BriBar.getProgress()/100f};
+        holder.lampColor.setBackgroundColor(Color.HSVToColor(hsb));
     }
 
     @Override
@@ -48,12 +58,120 @@ public class LampAdapter extends RecyclerView.Adapter<LampAdapter.ImageViewHolde
     public class ImageViewHolder extends RecyclerView.ViewHolder
     {
 
+        Button arrowBttn;
+        View lampColor;
+        SeekBar HueBar;
+        SeekBar SatBar;
+        SeekBar BriBar;
+        TextView HueText;
+        TextView SatText;
+        TextView BriText;
+        ConstraintLayout expandableView;
+        CardView cardview;
+
+
         public ImageViewHolder(View view)
         {
             super(view);
 
+            arrowBttn = view.findViewById(R.id.arrowBttn);
+            lampColor = view.findViewById(R.id.lampColor);
+            HueBar = view.findViewById(R.id.HBar);
+            SatBar = view.findViewById(R.id.SBar);
+            BriBar = view.findViewById(R.id.BBar);
+            HueText = view.findViewById(R.id.HueText);
+            SatText = view.findViewById(R.id.SatText);
+            BriText = view.findViewById(R.id.BriText);
+            expandableView = view.findViewById(R.id.expandableView);
+            cardview = view.findViewById(R.id.CardView);
 
 
+            arrowBttn.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+                    if (expandableView.getVisibility() == View.GONE)
+                    {
+                        TransitionManager.beginDelayedTransition(cardview, new AutoTransition());
+                        expandableView.setVisibility(View.VISIBLE);
+                        arrowBttn.setBackgroundResource(R.drawable.ic_keyboard_arrow_up_black_24dp);
+                    } else
+                    {
+                        TransitionManager.beginDelayedTransition(cardview, new AutoTransition());
+                        expandableView.setVisibility(View.GONE);
+                        arrowBttn.setBackgroundResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
+                    }
+                }
+            });
+
+            HueBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+            {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean b)
+                {
+                    HueText.setText(String.valueOf(progress));
+                    float[] hsb = {HueBar.getProgress(), SatBar.getProgress()/100f, BriBar.getProgress()/100f};
+                    lampColor.setBackgroundColor(Color.HSVToColor(hsb));
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar)
+                {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar)
+                {
+                }
+            });
+
+            SatBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+            {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean b)
+                {
+                    SatText.setText(String.valueOf(progress));
+                    float[] hsb = {HueBar.getProgress(), SatBar.getProgress()/100f, BriBar.getProgress()/100f};
+                    lampColor.setBackgroundColor(Color.HSVToColor(hsb));
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar)
+                {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar)
+                {
+
+                }
+            });
+
+            BriBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+            {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean b)
+                {
+                    BriText.setText(String.valueOf(progress));
+                    float[] hsb = {HueBar.getProgress(), SatBar.getProgress()/100f, BriBar.getProgress()/100f};
+                    lampColor.setBackgroundColor(Color.HSVToColor(hsb));
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar)
+                {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar)
+                {
+
+                }
+            });
         }
     }
 
--- app/src/main/java/com/example/hueapp/MainActivity.java
+++ app/src/main/java/com/example/hueapp/MainActivity.java
@@ -1,18 +1,43 @@
 package com.example.hueapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.example.hueapp.Adapters.LampAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private String TiesToken = "SpmILZFwnQhpVsCcUNWO0c5ObXXucxIHKzjNh5Lo";
     private String KirstenToken = "cRBAiU0YfOyx6dxOelEgngMYNVzyOw6yAJumocx1";
 
+    private RecyclerView recyclerView;
+    private LampAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<HueLamp> dataset;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.reyclerview);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        this.dataset = new ArrayList<>();
+        dataset.add(new HueLamp(true, 100, 0, 0, false));
+        dataset.add(new HueLamp(true, 100, 0, 0, false));
+
+
+        adapter = new LampAdapter(dataset);
+
+        recyclerView.setAdapter(adapter);
 
 
 
